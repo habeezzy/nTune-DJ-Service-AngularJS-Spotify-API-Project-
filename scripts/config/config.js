840daba870d958200a27f33a28b558ee255f02c7
habeezzy <mhabeebulah@gmail.com>
2017-12-15 10:32:02 -0500

    last commit

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -12,7 +12,7 @@
       template: "<playlist-component></playlist-component>"
     });
     SpotifyProvider.setClientId('fd36055cd7f8457cbc2e621683f0c86a');
-    SpotifyProvider.setAuthToken("BQBNH8QrDYlmU1Fglrpj_MHsjIENMyiMgpT7NgKn0wV_sOux0xvYUe_y5wNqVUlhqKUrALqE4dsHDsOKGyXeUM3mIcavWsk8rOw8uq5XBOARW-l9SaRCG-RejzD6n4PMbzTPPUabPhm-")
+    SpotifyProvider.setAuthToken("BQCmeksN6bLlUpLrnF3l7jO-Sl6uX5f8sLExfSZzahIoRo5wxD5eyYVWeBWEoFhDewdX4QoYvefTtm5KyWzvKvRTwC6OK_qs1eOLXxRmCFsUTdecxiOQKuniek2Sp190i-W95GJ55sT0qFI")
   })
 
 })();
